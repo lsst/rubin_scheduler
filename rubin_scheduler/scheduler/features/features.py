827d7360d420d40178f1b5edce888d5fd4253fa5
Lynne Jones <ljones.uw@gmail.com>
2024-04-09 16:31:49 -0700

    Update the NObservationsCurrentSeason feature, with unit test.

--- rubin_scheduler/scheduler/features/features.py
+++ rubin_scheduler/scheduler/features/features.py
@@ -464,8 +464,33 @@ class LastNObsTimes(BaseSurveyFeature):
 
 
 class NObservationsCurrentSeason(BaseSurveyFeature):
-    """Track how many observations have been taken in the current season
-    that meet criteria.
+    """Count the observations at each healpix, taken in the most
+    recent season, that meet filter, seeing and m5 criteria.
+
+    Useful for ensuring that "good quality" observations are acquired
+    in each season at each point in the survey footprint.
+
+    Parameters
+    ----------
+    filtername : `str`, optional
+        If None (default) count observations in any filter.
+        Otherwise, only count observations in the specified filter.
+    nside : `int`, optional
+        If None (default), use default nside for scheduler.
+        Otherwise, set nside for the map to nside.
+    seeing_fwhm_max : `float`, optional
+        If None (default), count observations up to any seeing value.
+        Otherwise, only count observations with better seeing (`FWHMeff`)
+        than `seeing_fwhm_max`. In arcseconds.
+    m5_penalty_max : `float`, optional
+        If None (default), count observations with any m5 value.
+        Otherwise, only count observations within this value of the
+        dark sky map at this pixel.
+        Only relevant if filtername is not None.
+    mjd_start : `float`, optional
+        If None, uses default survey_start_mjd for the start of the survey.
+        This defines the starting year for counting seasons, so should
+        be the start of the survey.
     """
 
     def __init__(
@@ -474,89 +499,188 @@ class NObservationsCurrentSeason(BaseSurveyFeature):
         nside=None,
         seeing_fwhm_max=None,
         m5_penalty_max=None,
-        mjd_start=1,
+        mjd_start=None,
     ):
-        self.filtername = filtername
         if nside is None:
-            self.nside = utils.set_default_nside()
-        else:
-            self.nside = nside
+            nside = utils.set_default_nside()
+        self.nside = nside
         self.seeing_fwhm_max = seeing_fwhm_max
+        self.filtername = filtername
         self.m5_penalty_max = m5_penalty_max
+        # If filtername not set, then we can't set m5_penalty_max.
+        if self.filtername is None and self.m5_penalty_max is not None:
+            warnings.warn("To use m5_penalty_max, filtername must be set. Disregarding m5_penalty_max.")
+            self.m5_penalty_max = None
 
-        self.feature = np.zeros(hp.nside2npix(nside), dtype=float)
+        # Get the dark sky map
         if self.filtername is not None:
             self.dark_map = dark_sky(nside)[filtername]
         self.ones = np.ones(hp.nside2npix(self.nside))
+
+        if mjd_start is None:
+            mjd_start = survey_start_mjd()
+        self.mjd_start = mjd_start
+
+        # Set up feature values - this includes the count in a given season
+        # Find the healpixels for each point on the sky
         self.ra, self.dec = _hpid2_ra_dec(nside, np.arange(hp.nside2npix(nside)))
-        self.season_map = calc_season(np.degrees(self.ra), mjd_start)
+        self.ra_deg = np.degrees(self.ra)
+
+        self.mjd = mjd_start
+        # Set up season_map. This should be the same as conditions.season_map
+        # but the important thing is that mjd_start matches.
+        self.season_map = calc_season(self.ra_deg, [self.mjd], self.mjd_start).flatten()
+        self.season = np.floor(self.season_map)
+
+        self.feature = np.zeros(hp.nside2npix(nside), dtype=float)
+        # Set bins for add_observations_array
         self.bins = np.arange(hp.nside2npix(nside) + 1) - 0.5
 
     def season_update(self, observation=None, conditions=None):
-        """clear the map anywhere the season has rolled over"""
-        if observation is not None:
-            current_season = calc_season(np.degrees(self.ra), observation["mjd"])
-        if conditions is not None:
-            current_season = calc_season(np.degrees(self.ra), conditions.mjd)
+        """Update the season_map to the current time.
 
-        # If the season has changed anywhere, set that count to zero
-        new_season = np.where((self.season_map - current_season) != 0)
-        self.feature[new_season] = 0
-        self.season_map = current_season
+        This assumes time increases monotonically in the conditions or
+        observations objects passed as arguments.
+        Using the 'mjd' from the conditions, where parts of the sky
+        have changed season (increasing in the `self.season_map` + 1)
+        the `self.feature` is cleared, in order to restart counting
+        observations in the new season.
+
+        Parameters
+        ----------
+        observation : `np.array`, (1,N)
+            Array of observation information, containing
+            `mjd` for the time. See
+            `rubin_scheduler.scheduler.utils.empty_observation`.
+        conditions : `rubin_scheduler.scheduler.Conditions`, optional
+            A conditions object, containing `mjd`.
+
+        Notes
+        -----
+        One of observations or conditions must be passed, but either
+        are options so this can be used with add_observation.
+        Most of the time, the updates will come from `conditions`.
+        """
+        mjd_now = None
+        mjd_from = None
+        if observation is not None:
+            mjd_now = observation["mjd"]
+            mjd_from = "observation"
+        # Prefer to use Conditions for the time.
+        if conditions is not None:
+            mjd_now = conditions.mjd
+            mjd_from = "conditions"
+        if mjd_now is None:
+            warnings.warn(
+                "Expected either a conditions or observations object. Not updating season_map.",
+                UserWarning,
+            )
+            return
+        if isinstance(mjd_now, np.ndarray) or isinstance(mjd_now, list):
+            mjd_now = mjd_now[0]
+        # Check that time doesn't go backwards.
+        # But only bother to warn if it's coming from conditions.
+        # Observations may look like they go backwards.
+        if mjd_now < self.mjd:
+            if mjd_from == "conditions":
+                warnings.warn(
+                    f"Time must flow forwards to track the "
+                    f"feature in {self.__class__.__name__}."
+                    f"Not updating season_map.",
+                    UserWarning,
+                )
+            # But just return without update in either case.
+            return
+        # Calculate updated season values.
+        updated_season = np.floor(self.season_map + (mjd_now - self.mjd_start) / 365.25)
+        # Clear feature where season increased by 1 (note 'floor' above).
+        self.feature[np.where(updated_season > self.season)] = 0
+        # Update to new time and season.
+        self.mjd = mjd_now
+        self.season = updated_season
 
     def add_observations_array(self, observations_array, observations_hpid):
+        # Update self.season to the most recent observation time
         self.season_update(observation=observations_array[-1])
 
-        check1 = np.zeros(observations_array.size, dtype=bool)
+        # Start assuming 'check' is all True, for all observations.
+        check = np.ones(observations_array.size, dtype=bool)
+
+        # Rule out the observations with seeing fwhmeff > limit
         if self.seeing_fwhm_max is not None:
-            check1[np.where(observations_array["FWHMeff"] <= self.seeing_fwhm_max)] = True
-        else:
-            check1[:] = True
+            check[np.where(observations_array["FWHMeff"] > self.seeing_fwhm_max)] = False
 
-        check2 = np.zeros(observations_array.size, dtype=bool)
+        # Rule out observations which are not in the desired filter
+        if self.filtername is not None:
+            check[np.where(observations_array["filter"] != self.filtername)] = False
+
+        # Convert the "check" array on observations_array into a
+        # "check" array on the hpids so we can evaluate healpix-level items
+        check_hpid_indxs = np.in1d(observations_hpid["ID"], observations_array["ID"][check])
+        # Set up a new valid/not valid flag, now on observations_hpid
+        check_hp = np.zeros(len(observations_hpid), dtype=bool)
+        # Set all observations which passed simpler tests above to True
+        check_hp[check_hpid_indxs] = True
+
+        hpids = observations_hpid["hpid"]
+
+        # This could be done once per observation, but to make life
+        # easier for index matching, let's just calculate season
+        # based on the hpid array.
+        # We only want to count observations that would fall within the
+        # current season, so calculate the season for each obs.
+        seasons = np.floor(
+            calc_season(
+                np.degrees(observations_hpid["RA"]),
+                observations_hpid["mjd"],
+                self.mjd_start,
+                calc_diagonal=True,
+            )
+        )
+        season_change = self.season[hpids] - seasons
+        check_hp = np.where(season_change != 0, False, check_hp)
+        # And check for m5_penalty_map
         if self.m5_penalty_max is not None:
-            hpid = ra_dec2_hpid(self.nside, observations_array["RA"], observations_array["dec"])
-            penalty = self.dark_map[hpid] - observations_array["fivesigmadepth"]
-            check2[np.where(penalty <= self.m5_penalty_max)] = True
-        else:
-            check2[:] = True
+            penalty = self.dark_map[hpids] - observations_hpid["fivesigmadepth"]
+            check_hp = np.where(penalty > self.m5_penalty_max, False, check_hp)
 
-        if self.filtername is None:
-            check3 = np.zeros(observations_array.size, dtype=bool)
-            check3[np.where(observations_array["filter"] == self.filter)] = True
-        else:
-            check3 = np.ones(observations_array.size, dtype=bool)
-
-        good_ids = observations_array[check1 & check2 % check3]["ID"]
-
-        indx = np.in1d(observations_hpid["ID"], observations_array["ID"][good_ids])
-
+        # Bin up the observations per hpid and add them to the feature.
         result, _be, _bn = binned_statistic(
-            observations_hpid["hpid"][indx],
-            observations_hpid["hpid"][indx],
+            observations_hpid["hpid"][check_hp],
+            observations_hpid["hpid"][check_hp],
             bins=self.bins,
             statistic=np.size,
         )
+        # Add the resulting observations to feature.
         self.feature += result
 
-    def add_observation(self, observation, indx=None):
+    def add_observation(self, observation, indx):
+        # Update the season map to the current time.
         self.season_update(observation=observation)
+        # Check if *this* observation is in the current season
+        # (This means we could add observations from the past, but
+        # would count it against the current season).
+        season_obs = np.floor(self.season_map[indx] + (observation["mjd"] - self.mjd_start) / 365.25)
+        this_season_indx = np.array(indx)[np.where(season_obs == self.season[indx])]
 
+        # Check if seeing is good enough.
         if self.seeing_fwhm_max is not None:
-            check1 = observation["FWHMeff"] <= self.seeing_fwhm_max
+            check = observation["FWHMeff"] <= self.seeing_fwhm_max
         else:
-            check1 = True
+            check = True
 
-        if self.m5_penalty_max is not None:
-            hpid = ra_dec2_hpid(self.nside, observation["RA"], observation["dec"])
-            penalty = self.dark_map[hpid] - observation["fivesigmadepth"]
-            check2 = penalty <= self.m5_penalty_max
-        else:
-            check2 = True
+        # Check if observation is in the right filter
+        if self.filtername is not None and check:
+            if observation["filter"] != self.filtername:
+                check = False
+            else:
+                # Check if observation in correct filter and deep enough.
+                if self.m5_penalty_max is not None:
+                    penalty = self.dark_map[this_season_indx] - observation["fivesigmadepth"]
+                    this_season_indx = this_season_indx[np.where(penalty <= self.m5_penalty_max)]
 
-        if check1 & check2:
-            if self.filtername is None or observation["filter"][0] in self.filtername:
-                self.feature[indx] += 1
+        if check:
+            self.feature[this_season_indx] += 1
 
 
 class CoaddedDepth(BaseSurveyFeature):
--- tests/scheduler/test_features.py
+++ tests/scheduler/test_features.py
@@ -1,10 +1,13 @@
 import unittest
+from copy import deepcopy
 
 import numpy as np
 
 import rubin_scheduler.scheduler.features as features
 from rubin_scheduler.scheduler.model_observatory import ModelObservatory
-from rubin_scheduler.scheduler.utils import empty_observation
+from rubin_scheduler.scheduler.utils import HpInLsstFov, empty_observation
+from rubin_scheduler.skybrightness_pre import dark_sky
+from rubin_scheduler.utils import survey_start_mjd
 
 
 class TestFeatures(unittest.TestCase):
@@ -106,6 +109,182 @@ class TestFeatures(unittest.TestCase):
 
         note_last_observed.add_observation(observation=observation)
         assert note_last_observed.feature == observation["mjd"]
+
+    def test_NObservationsCurrentSeason(self):
+        # Start with basic NObservationsCurrentSeason - no restrictions
+        mjd_start = survey_start_mjd()
+        nside = 64
+        season_feature = features.NObservationsCurrentSeason(nside=nside, mjd_start=mjd_start)
+        # Check that season map changes as expected with updates in Conditions
+        conditions = features.Conditions(nside=nside)
+        conditions.mjd = mjd_start
+        season = deepcopy(season_feature.season)
+        # with same time, should have same seasons.
+        season_feature.season_update(conditions=conditions)
+        self.assertTrue(np.all(season_feature.season == season))
+        # Advancing time should update seasons in some parts of the sky
+        # And leave it the same in other parts of the sky
+        conditions.mjd = mjd_start + 100
+        season_feature.season_update(conditions=conditions)
+        self.assertTrue(np.any(season_feature.season != season))
+        self.assertTrue(np.any(season_feature.season == season))
+        season = deepcopy(season_feature.season)
+        # Do we send a warning if time goes backwards
+        # and keep the season and feature the same
+        conditions.mjd = mjd_start
+        with self.assertWarns(UserWarning):
+            season_feature.season_update(conditions=conditions)
+        self.assertTrue(np.all(season_feature.season == season))
+        # And do the same for observations - check update_seasons works
+        # Make some observations
+        observations = [empty_observation(), empty_observation()]
+        observations[0]["mjd"] = mjd_start
+        observations[0]["ID"] = 0
+        observations[1]["mjd"] = mjd_start + 100
+        observations[1]["ID"] = 1
+        season_feature = features.NObservationsCurrentSeason(nside=16, mjd_start=mjd_start)
+        season = deepcopy(season_feature.season)
+        season_feature.season_update(observation=observations[0])
+        self.assertTrue(np.all(season_feature.season == season))
+        season_feature.season_update(observation=observations[1])
+        self.assertTrue(np.any(season_feature.season != season))
+        self.assertTrue(np.any(season_feature.season == season))
+        season = deepcopy(season_feature.season)
+        # Make time go backwards .. should NOT warn but should not
+        # update feature.
+        # with self.assertWarns(UserWarning):
+        #    season_feature.season_update(observation=observations[0])
+        self.assertTrue(np.all(season_feature.season == season))
+
+        # Now check that the _feature) works as expected when we
+        # add_observation (i.e. we now care about location and healpix)
+        for obs in observations:
+            obs["RA"] = np.radians(30)
+            obs["dec"] = np.radians(-20)
+        observations[1]["mjd"] = mjd_start + 0.3
+        pointing2indx = HpInLsstFov(nside=nside)
+        indxs = pointing2indx(observations[0]["RA"], observations[0]["dec"])
+        # first with no restrictions
+        season_feature = features.NObservationsCurrentSeason(nside=nside, mjd_start=mjd_start)
+        self.assertTrue(np.all(season_feature.feature == 0))
+        season_feature.add_observation(observations[0], indxs)
+        # Assert that we added an observation at each of the indexes
+        self.assertTrue(np.all(season_feature.feature[indxs] == 1))
+        # and nowhere else
+        self.assertTrue(np.all(np.delete(season_feature.feature, indxs) == 0))
+        # Add another observation within a day
+        season_feature.add_observation(observations[1], indxs)
+        # Assert that we added an observation at each of the indexes
+        self.assertTrue(np.all(season_feature.feature[indxs] == 2))
+        # and nowhere else
+        self.assertTrue(np.all(np.delete(season_feature.feature, indxs) == 0))
+        # Add an observation at a different point on the sky, but where
+        # season should not turn oer for ra above yet.
+        observations.append(empty_observation())
+        observations[-1]["mjd"] = mjd_start + 10
+        observations[-1]["RA"] = np.radians(50)
+        observations[-1]["dec"] = np.radians(-20)
+        observations[-1]["ID"] = len(observations) - 1
+        indxs2 = pointing2indx(observations[-1]["RA"], observations[-1]["dec"])
+        season_feature.add_observation(observations[-1], indxs2)
+        self.assertTrue(np.all(season_feature.feature[indxs2] == 1))
+        self.assertTrue(np.all(season_feature.feature[indxs] == 2))
+        # Check that if we fast-forward to a future year, and add an
+        # observation that everything reset to 1 or 0.
+        observation = observations[0]
+        observation["mjd"] = mjd_start + 365.25 * 2
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature[indxs] == 1))
+        self.assertTrue(np.all(np.delete(season_feature.feature, indxs) == 0))
+
+        # Set up to check add_observations_array - use observations from above.
+        observations[0]["mjd"] = mjd_start
+        observations_array = np.empty(len(observations), dtype=observations[0].dtype)
+        for i, obs in enumerate(observations):
+            observations_array[i] = obs
+        # Build observations_hpids_array.
+        # Find list of lists of healpixels
+        # (should match [indxs, indxs, indxs2] from above)
+        list_of_hpids = pointing2indx(observations_array["RA"], observations_array["dec"])
+        indxs_list = [indxs, indxs, indxs2]
+        for hi, ii in zip(list_of_hpids, indxs_list):
+            self.assertTrue(set(hi) == set(ii))
+        # Unravel list-of-lists (list_of_hpids) to match against observations
+        hpids = []
+        big_array_indx = []
+        for i, indxs in enumerate(list_of_hpids):
+            for indx in indxs:
+                hpids.append(indx)
+                big_array_indx.append(i)
+        hpids = np.array(hpids, dtype=[("hpid", int)])
+        # Set up format / dtype for observations_hpids_array
+        names = list(observations_array.dtype.names)
+        types = [observations_array[name].dtype for name in names]
+        names.append(hpids.dtype.names[0])
+        types.append(hpids["hpid"].dtype)
+        ndt = list(zip(names, types))
+        observations_hpids_array = np.empty(hpids.size, dtype=ndt)
+        # Populate observations_hpid_array - big_array_indx points
+        # between index in observations_array and index in hpid
+        observations_hpids_array[list(observations_array.dtype.names)] = observations_array[big_array_indx]
+        observations_hpids_array[hpids.dtype.names[0]] = hpids
+        # Now check that adding observations to feature via
+        # add_observations_array results in the same feature
+        feature_obs = features.NObservationsCurrentSeason(nside=nside, mjd_start=mjd_start)
+        for obs, idx in zip(observations, indxs_list):
+            feature_obs.add_observation(obs, idx)
+        feature_arr = features.NObservationsCurrentSeason(nside=nside, mjd_start=mjd_start)
+        feature_arr.add_observations_array(observations_array, observations_hpids_array)
+        self.assertTrue(np.all(feature_obs.season == feature_arr.season))
+        self.assertTrue(np.all(feature_obs.feature == feature_arr.feature))
+
+        # Check that feature works as expected when adding requirements.
+        # Add seeing requirement.
+        season_feature = features.NObservationsCurrentSeason(
+            nside=nside, mjd_start=mjd_start, seeing_fwhm_max=1.0
+        )
+        observation = empty_observation()
+        observation["mjd"] = mjd_start
+        observation["RA"] = np.radians(30)
+        observation["dec"] = np.radians(-20)
+        # Don't add if seeing is too bad
+        observation["FWHMeff"] = 1.5
+        pointing2indx = HpInLsstFov(nside=nside)
+        indxs = pointing2indx(observation["RA"], observation["dec"])
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature == 0))
+        # Do add if seeing is good enough
+        observation["FWHMeff"] = 0.5
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature[indxs] == 1))
+        # Add filter requirement.
+        season_feature = features.NObservationsCurrentSeason(
+            nside=nside, mjd_start=mjd_start, seeing_fwhm_max=1.0, filtername="r"
+        )
+        observation["filter"] = "g"
+        # Don't add if in the wrong filter
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature == 0))
+        # Do add if correct filter and good seeing.
+        observation["filter"] = "r"
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature[indxs] == 1))
+        # Add m5 requirement.
+        season_feature = features.NObservationsCurrentSeason(
+            nside=nside, mjd_start=mjd_start, seeing_fwhm_max=1.0, filtername="r", m5_penalty_max=0
+        )
+        dark_map = dark_sky(nside)["r"]
+        # Don't add if too bright
+        observation["fivesigmadepth"] = dark_map[indxs].min() - 10
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature == 0))
+        # Do add if faint enough
+        observation["fivesigmadepth"] = dark_map[indxs].max() + 10
+        season_feature.add_observation(observation, indxs)
+        self.assertTrue(np.all(season_feature.feature[indxs] == 1))
+        # Should test also that add_observations_array works
+        # in these cases with added requirements .. but will leave it
+        # to the "restore" test in test_utils.py.
 
 
 if __name__ == "__main__":
