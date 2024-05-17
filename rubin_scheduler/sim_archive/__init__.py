73ca6ac196ddc216b53e2ad922fc92ca3cc143a2
Eric Neilsen <neilsen@fnal.gov>
2024-05-17 13:42:51 -0700

    generate script to run prenight simulations

--- rubin_scheduler/sim_archive/__init__.py
+++ rubin_scheduler/sim_archive/__init__.py
@@ -4,5 +4,6 @@ import logging
 HAVE_LSST_RESOURCES = importlib.util.find_spec("lsst") and importlib.util.find_spec("lsst.resources")
 if HAVE_LSST_RESOURCES:
     from .sim_archive import *
+    from .prenight import *
 else:
     logging.error("rubin_scheduler.sim_archive requires lsst.resources.")
