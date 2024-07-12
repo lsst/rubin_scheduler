19a530301693df5c24e43d8914fc5f516674e08e
Eric Neilsen <neilsen@fnal.gov>
2024-07-12 13:31:10 -0500

    let tests pass if lsst.resources is absent

--- tests/sim_archive/test_prenight.py
+++ tests/sim_archive/test_prenight.py
@@ -2,7 +2,6 @@ import importlib.util
 import unittest
 from tempfile import TemporaryDirectory
 
-from rubin_scheduler.sim_archive import prenight_sim_cli
 
 try:
     from lsst.resources import ResourcePath
@@ -10,6 +9,9 @@ try:
     HAVE_RESOURCES = True
 except ModuleNotFoundError:
     HAVE_RESOURCES = False
+
+if HAVE_RESOURCES:
+    from rubin_scheduler.sim_archive import prenight_sim_cli
 
 # We need rubin_sim to get the baseline sim
 # Tooling prefers checking that it exists using importlib rather
