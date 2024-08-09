3af8eaa0ac1bb781aab539d5af8f3768fbb2417e
Lynne Jones <ljones.uw@gmail.com>
2024-08-08 17:24:32 -0700

    Allow use of slewtime estimate when filtername is not specified.

--- rubin_scheduler/scheduler/model_observatory/kinem_model.py
+++ rubin_scheduler/scheduler/model_observatory/kinem_model.py
@@ -458,7 +458,9 @@ class KinemModel:
             The number of seconds between the two specified exposures.
             Will be np.nan or np.inf if slew is not possible.
         """
-        if filtername not in self.mounted_filters:
+        if filtername is None:
+            filtername = self.current_filter
+        elif filtername not in self.mounted_filters:
             return np.nan
 
         # Don't trust folks to do pa calculation correctly, if both
