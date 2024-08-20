57e30e614a0e32cceb49f8325abf527d53fc9006
Lynne Jones <ljones.uw@gmail.com>
2024-08-20 02:51:04 -0700

    Set the Pair/Blob survey name automatically, correctly.

--- rubin_scheduler/scheduler/surveys/surveys.py
+++ rubin_scheduler/scheduler/surveys/surveys.py
@@ -119,14 +119,18 @@ class BlobSurvey(GreedySurvey):
         The approximate slewtime between neerby fields (seconds). Used
         to calculate how many observations can be taken in the
         desired time block.
+    filter_change_approx : `float`
+         The approximate time it takes to change filters (seconds).
+    read_approx : `float`
+        The approximate time required to readout the camera (seconds).
+    exptime : `float`
+        The total on-sky exposure time per visit.
     nexp : `int`
         The number of exposures to take in a visit.
     exp_dict : `dict`
         If set, should have keys of filtername and values of ints that
         are the nuber of exposures to take per visit. For estimating
         block time, nexp is still used.
-    filter_change_approx : `float`
-         The approximate time it takes to change filters (seconds).
     ideal_pair_time : `float`
         The ideal time gap wanted between observations to the same
         pointing (minutes)
@@ -192,8 +196,10 @@ class BlobSurvey(GreedySurvey):
         self.filtername1 = filtername1
         self.filtername2 = filtername2
 
+        self.ideal_pair_time = ideal_pair_time
+
         if survey_name is None:
-            survey_name = self._generate_survey_name()
+            self._generate_survey_name()
 
         super(BlobSurvey, self).__init__(
             basis_functions=basis_functions,
@@ -209,7 +215,7 @@ class BlobSurvey(GreedySurvey):
             camera=camera,
             area_required=area_required,
             fields=fields,
-            survey_name=survey_name,
+            survey_name=self.survey_name,
             scheduler_note=scheduler_note,
         )
         self.flush_time = flush_time / 60.0 / 24.0  # convert to days
@@ -237,10 +243,10 @@ class BlobSurvey(GreedySurvey):
         # then repeat.)
         if filtername2 is not None:
             self.time_needed = (
-                (ideal_pair_time * 60.0 * 2.0 + exptime + read_approx + filter_change_approx) / 24.0 / 3600.0
+                (self.ideal_pair_time * 60.0 * 2.0 + self.exptime + self.read_approx + filter_change_approx) / 24.0 / 3600.0
             )  # Days
         else:
-            self.time_needed = (ideal_pair_time * 60.0 + exptime + read_approx) / 24.0 / 3600.0  # Days
+            self.time_needed = (self.ideal_pair_time * 60.0 + self.exptime + self.read_approx) / 24.0 / 3600.0  # Days
         self.filter_set = set(filtername1)
         if filtername2 is None:
             self.filter2_set = self.filter_set
@@ -250,7 +256,6 @@ class BlobSurvey(GreedySurvey):
         self.ra, self.dec = _hpid2_ra_dec(self.nside, self.hpids)
 
         self.counter = 1  # start at 1, because 0 is default in empty obs
-        self.ideal_pair_time = ideal_pair_time
 
         self.pixarea = hp.nside2pixarea(self.nside, degrees=True)
 
@@ -259,7 +264,9 @@ class BlobSurvey(GreedySurvey):
             self.filtername = self.filtername1
 
     def _generate_survey_name(self):
-        self.survey_name = f"Blob survey {self.filtername1}"
+        self.survey_name = "Pair survey"
+        self.survey_name += f" {self.ideal_pair_time :.1f}"
+        self.survey_name += f" {self.filtername1}"
         if self.filtername2 is None:
             self.survey_name += f"_{self.filtername1}"
         else:
