88ffab4bbd5181bc75a06e0080248c0bb934b905
Lynne Jones <ljones.uw@gmail.com>
2024-08-21 13:40:22 -0700

    Make the test survey start date land within the available test skybrightness files.

--- tests/scheduler/test_comcam_surveys.py
+++ tests/scheduler/test_comcam_surveys.py
@@ -1,10 +1,8 @@
-import os
 import unittest
 
 import numpy as np
 from astropy.time import Time
 
-from rubin_scheduler.data import get_data_dir
 from rubin_scheduler.scheduler import sim_runner
 from rubin_scheduler.scheduler.example import (
     get_comcam_sv_schedulers,
@@ -13,28 +11,29 @@ from rubin_scheduler.scheduler.example import (
     update_model_observatory_sunset,
 )
 from rubin_scheduler.scheduler.schedulers import ComCamFilterSched
-
-SAMPLE_BIG_DATA_FILE = os.path.join(get_data_dir(), "scheduler/dust_maps/dust_nside_32.npz")
+from rubin_scheduler.utils import survey_start_mjd
 
 
 class TestComCamSurveys(unittest.TestCase):
 
-    @unittest.skipUnless(os.path.isfile(SAMPLE_BIG_DATA_FILE), "Test data not available.")
     def test_model_observatory_conveniences(self):
         """Test the model observatory convenience functions."""
 
         # Just check that we can acquire a model observatory
         # and it is set up for the date expected.
-        dayobs = "2024-09-09"
-        survey_start = Time(f"{dayobs}T12:00:00", format="isot", scale="utc").mjd
+        # To use the skybrightness files available for small tests,
+        # use survey_start_mjd
+        survey_start = survey_start_mjd()
+        survey_start = np.floor(survey_start) + 0.5
+        dayobs = Time(survey_start, format="mjd", scale="utc").iso[:10]
         observatory = get_model_observatory(dayobs=dayobs, survey_start=survey_start)
         conditions = observatory.return_conditions()
         assert conditions.mjd == observatory.mjd
         # The model observatory automatically advanced to -12 deg sunset
         assert (conditions.mjd - survey_start) < 1
 
-        new_dayobs = "2024-09-12"
-        newday = Time(f"{new_dayobs}T12:00:00", format="isot", scale="utc").mjd
+        newday = survey_start + 4
+        new_dayobs = Time(newday, format="mjd", scale="utc").iso[:10]
         observatory = get_model_observatory(dayobs=new_dayobs, survey_start=survey_start)
         conditions = observatory.return_conditions()
         assert (conditions.mjd - newday) < 1
@@ -51,10 +50,11 @@ class TestComCamSurveys(unittest.TestCase):
         # This is likely to change as we go into commissioning,
         # so mostly I'm just going to test that the end result is
         # a usable scheduler
-        dayobs = "2024-09-09"
-        survey_start_mjd = Time(f"{dayobs}T12:00:00", format="isot", scale="utc").mjd
+        survey_start = survey_start_mjd()
+        survey_start = np.floor(survey_start) + 0.5
+        dayobs = Time(survey_start, format="mjd", scale="utc").iso[:10]
         scheduler, filter_scheduler = get_comcam_sv_schedulers()
-        observatory = get_model_observatory(dayobs=dayobs, survey_start=survey_start_mjd)
+        observatory = get_model_observatory(dayobs=dayobs, survey_start=survey_start)
         observatory = update_model_observatory_sunset(observatory, filter_scheduler)
 
         observatory, scheduler, observations = sim_runner(
@@ -63,5 +63,5 @@ class TestComCamSurveys(unittest.TestCase):
         assert len(observations) > 24000
         sv_fields = set(np.unique(observations["scheduler_note"]))
         all_sv_fields = set(list(get_sv_fields().keys()))
-        # Probably won't observe all of the fields, but can do some.
+        # Probably won't observe all of the fields.
         assert sv_fields.intersection(all_sv_fields) == sv_fields
