74d14922a5808bfaba21749b887f846cbc782281
Peter Yoachim <yoachim@uw.edu>
2024-08-29 14:03:12 -0700

    random filter detailer

--- rubin_scheduler/scheduler/detailers/detailer.py
+++ rubin_scheduler/scheduler/detailers/detailer.py
@@ -11,6 +11,7 @@ __all__ = (
     "FixedSkyAngleDetailer",
     "ParallacticRotationDetailer",
     "FlushByDetailer",
+    "RandomFilterDetailer",
 )
 
 import copy
@@ -99,6 +100,26 @@ class FlushByDetailer(BaseDetailer):
     def __call__(self, observation_list, conditions):
         for obs in observation_list:
             obs["flush_by_mjd"] = conditions.mjd + self.flush_time
+        return observation_list
+
+
+class RandomFilterDetailer(BaseDetailer):
+    """Pick a random filter for the observations"""
+
+    def __init__(self, filters="riz", nights_to_prep=10000, seed=42):
+        self.survey_features = []
+
+        rng = np.random.default_rng(seed)
+        self.night2filter_int = rng.integers(low=0, high=len(filters), size=nights_to_prep)
+
+        self.filter_dict = {}
+        for i, filtername in enumerate(filters):
+            self.filter_dict[i] = filtername
+
+    def __call__(self, observation_list, conditions):
+
+        for obs in observation_list:
+            obs["filter"] = self.filter_dict[self.night2filter_int[conditions.night]]
         return observation_list
 
 
