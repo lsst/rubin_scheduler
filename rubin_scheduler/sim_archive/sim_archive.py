32767d9f0530563f0fa158e4d58550e1e5def160
Eric Neilsen <neilsen@fnal.gov>
2024-10-10 08:06:50 -0700

    add compile_sim_archive_metadata_resource command

--- rubin_scheduler/sim_archive/sim_archive.py
+++ rubin_scheduler/sim_archive/sim_archive.py
@@ -25,6 +25,7 @@ from contextlib import redirect_stdout
 from numbers import Integral, Number
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from urllib.parse import urlparse
 
 import numpy as np
 import pandas as pd
@@ -352,7 +353,9 @@ def _build_archived_sim_label(base_uri, metadata_resource, metadata):
     return label
 
 
-def read_archived_sim_metadata(base_uri, latest=None, num_nights=5):
+def read_archived_sim_metadata(
+    base_uri, latest=None, num_nights=5, compilation_resource=None, verify_compilation=False
+):
     """Read metadata for a time range of archived opsim output.
 
     Parameters
@@ -367,6 +370,13 @@ def read_archived_sim_metadata(base_uri, latest=None, num_nights=5):
         Default is today.
     num_nights : `int`
         The number of nights of the date window to load.
+    compilation_resource : `ResourcePath` or `str` or  `None`
+        The ResourcePath to an hdf5 compilation of metadata.
+    verify_compilation : `bool`
+        Verify that metadata in compilation corresponds to an existing
+        resource, and the all existing resources have metadata. Defaults
+        to False, which will work provided that the compilation is complete
+        and correct up to the date of the last simulation it includes.
 
     Returns
     -------
@@ -376,19 +386,57 @@ def read_archived_sim_metadata(base_uri, latest=None, num_nights=5):
     latest_mjd = int(Time.now().mjd if latest is None else Time(latest).mjd)
     earliest_mjd = int(latest_mjd - (num_nights - 1))
 
+    compilation = {}
+    compiled_uris_by_date = {}
+    max_compiled_date = "1900-01-01"
+    if compilation_resource is not None:
+        try:
+            compilation.update(read_sim_metadata_from_hdf(compilation_resource))
+            for uri in compilation:
+                iso_date = Path(urlparse(uri).path).parts[-2]
+                if iso_date not in compiled_uris_by_date:
+                    compiled_uris_by_date[iso_date] = []
+                compiled_uris_by_date[iso_date].append(uri)
+                max_compiled_date = max(max_compiled_date, iso_date)
+        except FileNotFoundError:
+            pass
+
     all_metadata = {}
     for mjd in range(earliest_mjd, latest_mjd + 1):
         iso_date = Time(mjd, format="mjd").iso[:10]
-        date_resource = ResourcePath(base_uri).join(iso_date, forceDirectory=True)
-        if date_resource.exists():
-            for base_dir, found_dirs, found_files in date_resource.walk(file_filter=r".*sim_metadata.yaml"):
-                for found_file in found_files:
-                    found_resource = ResourcePath(base_dir).join(found_file)
-                    these_metadata = yaml.safe_load(found_resource.read().decode("utf-8"))
-                    these_metadata["label"] = _build_archived_sim_label(
-                        base_uri, found_resource, these_metadata
-                    )
-                    all_metadata[str(found_resource.dirname())] = these_metadata
+
+        # Make the comparison >=, rather than >, so
+        # it won't miss sims in which the compilation does not complete
+        # the final date.
+        if verify_compilation or (iso_date >= max_compiled_date):
+            date_resource = ResourcePath(base_uri).join(iso_date, forceDirectory=True)
+            if date_resource.exists():
+                for base_dir, found_dirs, found_files in date_resource.walk(
+                    file_filter=r".*sim_metadata.yaml"
+                ):
+                    for found_file in found_files:
+                        found_resource = ResourcePath(base_dir).join(found_file)
+                        sim_uri = str(found_resource.dirname())
+                        if sim_uri in compilation:
+                            these_metadata = compilation[sim_uri]
+                        else:
+                            these_metadata = yaml.safe_load(found_resource.read().decode("utf-8"))
+                            these_metadata["label"] = _build_archived_sim_label(
+                                base_uri, found_resource, these_metadata
+                            )
+                            if iso_date < max_compiled_date:
+                                print(f"Simulation at {sim_uri} expected but not found in compilation.")
+                        all_metadata[sim_uri] = these_metadata
+        else:
+            if iso_date in compiled_uris_by_date:
+                for sim_uri in compiled_uris_by_date[iso_date]:
+                    all_metadata[sim_uri] = compilation[sim_uri]
+
+        if verify_compilation:
+            if iso_date in compiled_uris_by_date:
+                for sim_uri in compiled_uris_by_date[iso_date]:
+                    if sim_uri not in all_metadata:
+                        print(f"Simulation at {sim_uri} in compiled metadata but not archive.")
 
     return all_metadata
 
@@ -486,7 +534,7 @@ def make_sim_archive_cli(*args):
 
 
 def compile_sim_metadata(
-    archive_uri: str, compilation_resource: str | ResourcePath, num_nights: int = 10000
+    archive_uri: str, compilation_resource: str | ResourcePath, num_nights: int = 10000, append=False
 ) -> str:
     """Read sim archive metadata and export it to tables in an hdf5 files.
 
@@ -498,6 +546,10 @@ def compile_sim_metadata(
         Resource for hdf5 file to be written to
     num_nights : `int`, optional
         Number of nights to include, by default 10000
+    append : `bool`, optional
+        Do not rebuild the whole compilation, but instead reread what is
+        there already, and add new metadata after the last date already
+        include. Defaults to False.
 
     Returns
     -------
@@ -505,7 +557,12 @@ def compile_sim_metadata(
         The resource to which the hdf5 file was written.
     """
 
-    sim_metadata = rubin_scheduler.sim_archive.read_archived_sim_metadata(archive_uri, num_nights=num_nights)
+    if append:
+        sim_metadata = read_archived_sim_metadata(
+            archive_uri, num_nights=num_nights, compilation_resource=compilation_resource
+        )
+    else:
+        sim_metadata = read_archived_sim_metadata(archive_uri, num_nights=num_nights)
 
     sim_rows = []
     file_rows = []
@@ -551,6 +608,7 @@ def compile_sim_metadata(
             compilation_resource = ResourcePath(compilation_resource)
 
         assert isinstance(compilation_resource, ResourcePath)
+        LOGGER.info(f"Writing metadata compilation to {compilation_resource}")
         compilation_resource.write(hdf_bytes)
 
     return compilation_resource
@@ -572,7 +630,7 @@ def read_sim_metadata_from_hdf(compilation_resource: str | ResourcePath) -> dict
     """
 
     if isinstance(compilation_resource, str):
-        compilation_resource = ResourcePath(compilation_resounce)
+        compilation_resource = ResourcePath(compilation_resource)
     assert isinstance(compilation_resource, ResourcePath)
 
     with compilation_resource.as_local() as local_compilation_resource:
@@ -701,13 +759,19 @@ def verify_compiled_sim_metadata(
                         "compiled_value": "MISSING",
                     }
                 )
-                raise
 
     # Test that everything in the compilation has a corresponding matching
     # entry in direct_sim_metadata.
     for sim_uri in compiled_sim_metadata:
         for key in compiled_sim_metadata[sim_uri]:
-            if key not in direct_sim_metadata[sim_uri]:
+            if sim_uri not in direct_sim_metadata:
+                differences.append(
+                    {
+                        "sim_uri": sim_uri,
+                        "direct_value": "MISSING",
+                    }
+                )
+            elif key not in direct_sim_metadata[sim_uri]:
                 differences.append(
                     {
                         "sim_uri": sim_uri,
@@ -834,3 +898,37 @@ def drive_sim(
 
     results = sim_results + (resource_path,)
     return results
+
+
+def compile_sim_archive_metadata_cli(*args):
+    parser = argparse.ArgumentParser(description="Create a metadata compilation HDF5 file at a URI")
+    parser.add_argument(
+        "--compilation_uri",
+        type=str,
+        default=None,
+        help="The URI of the metadata archive compilation to write. "
+        + "Defaults to compilation_metadate.h5 in the archive.",
+    )
+    parser.add_argument(
+        "--archive_base_uri",
+        type=str,
+        default="s3://rubin:rubin-scheduler-prenight/opsim/",
+        help="Base URI for the archive",
+    )
+    parser.add_argument(
+        "--append",
+        action="store_true",
+        help="Do not rebuild the whole compilation, "
+        + "but add new simulations with dates after the last current entry.",
+    )
+
+    arg_values = parser.parse_args() if len(args) == 0 else parser.parse_args(args)
+    archive_uri = arg_values.archive_base_uri
+    compilation_uri = arg_values.compilation_uri
+    append = arg_values.append
+    if compilation_uri is None:
+        compilation_resource = ResourcePath(archive_uri).join("compiled_metadata_cache.h5")
+    else:
+        compilation_resource = ResourcePath(compilation_uri)
+
+    compilation_resource = compile_sim_metadata(archive_uri, compilation_resource, append=append)
