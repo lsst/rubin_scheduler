7b063fb006831a6bc55087a3d216d784b5380602
Peter Yoachim <yoachim@uw.edu>
2024-10-15 15:08:00 -0700

    make sure example runs at higher nside

--- tests/scheduler/test_example.py
+++ tests/scheduler/test_example.py
@@ -59,6 +59,49 @@ class TestExample(unittest.TestCase):
             # If this fails, time to add something to notes_to_check
             assert note in u_notes
 
+    @unittest.skipUnless(
+        os.path.isfile(os.path.join(get_data_dir(), "scheduler/dust_maps/dust_nside_32.npz")),
+        "Test data not available.",
+    )
+    def test_example_64(self):
+        """Test the example scheduler executes all the expected surveys 
+        and can run on a different nside."""
+        mjd_start = SURVEY_START_MJD
+        scheduler = example_scheduler(mjd_start=mjd_start, nside=64)
+        observatory, scheduler, observations = run_sched(
+            scheduler, mjd_start=mjd_start, survey_length=2, nside=64
+        )
+        u_notes = np.unique(observations["scheduler_note"])
+
+        # Note that some of these may change and need to be updated if
+        # survey start date changes, e.g., different DDFs in season,
+        # or different lunar phase means different filters get picked
+        # for the blobs
+        notes_to_check = [
+            "DD:COSMOS",
+            "blob_long, gr, a",
+            "blob_long, gr, b",
+            "greedy",
+            "long",
+            "pair_15, iz, a",
+            "pair_15, iz, b",
+            "pair_15, ri, a",
+            "pair_15, ri, b",
+            "pair_33, ug, a",
+            "pair_33, ug, b",
+            "twilight_near_sun, 0",
+            "twilight_near_sun, 1",
+            "twilight_near_sun, 2",
+            "twilight_near_sun, 3",
+        ]
+
+        for note in notes_to_check:
+            assert note in u_notes
+
+        for note in u_notes:
+            # If this fails, time to add something to notes_to_check
+            assert note in u_notes
+
 
 if __name__ == "__main__":
     unittest.main()
