ea53f3f122ee2d0c3dc6dbea1bdedd65305e6614
Eric Neilsen <neilsen@fnal.gov>
2025-03-14 07:43:46 -0700

    Use EFD to get versions for running pre-night sims

--- batch/run_prenight_sims.sh
+++ batch/run_prenight_sims.sh
@@ -24,11 +24,35 @@ fi
 
 source /sdf/group/rubin/sw/w_latest/loadLSST.sh
 conda activate /sdf/data/rubin/shared/scheduler/envs/prenight
+
+
+TS_CONFIG_OCS_HASH=$(obs_version_at_time ts_config_ocs)
+TS_FBS_UTILS_TAG=$(curl -s https://api.github.com/repos/lsst-ts/ts_fbs_utils/tags | jq -r '.[].name' | egrep '^v[0-9]+.[0-9]+.[0-9]+$' | sort -V | tail -1)
+
 export AWS_PROFILE=prenight
 WORK_DIR=$(date '+/sdf/data/rubin/shared/scheduler/prenight/work/run_prenight_sims/%Y-%m-%dT%H%M%S' --utc)
 echo "Working in $WORK_DIR"
 mkdir ${WORK_DIR}
 cd ${WORK_DIR}
+
+# Get ts_ocs_config
+curl --output ts_ocs_config.zip https://github.com/lsst-ts/ts_config_ocs/archive/${TS_CONFIG_OCS_HASH}.zip
+unzip ts_ocs_config.zip
+
+# Install required python packages
+PACKAGE_DIR=$(readlink -f ${WORK_DIR}/packages)
+mkdir ${PACKAGE_DIR}
+
+# Get the scheduler version from the EFD and install it.
+RUBIN_SCHEDULER_TAG=v$(obs_version_at_time rubin_scheduler)
+pip install --no-deps --target=${PACKAGE_DIR} git+https://github.com/lsst/rubin_scheduler.git@${RUBIN_SCHEDULER_TAG}
+
+# Cannot get ts_fbs_utils from the EFD, so just guess the highest semantic version tag in the repo.
+TS_FBS_UTILS_TAG=$(curl -s https://api.github.com/repos/lsst-ts/ts_fbs_utils/tags | jq -r '.[].name' | egrep '^v[0-9]+.[0-9]+.[0-9]+$' | sort -V | tail -1)
+pip install --no-deps --target=${PACKAGE_DIR} git+https://github.com/lsst-ts/ts_fbs_utils.git@${TS_FBS_UTILS_TAG}
+
+export PYTHONPATH=${PACKAGE_DIR}:${PYTHONPATH}
+
 printenv > env.out
 prenight_sim --scheduler auxtel.pickle.xz --opsim None --repo "https://github.com/lsst-ts/ts_config_ocs.git" --script "Scheduler/feature_scheduler/auxtel/fbs_config_image_photocal_survey.py" --branch main
 echo "******* END of run_prenight_sims.sh *********"
