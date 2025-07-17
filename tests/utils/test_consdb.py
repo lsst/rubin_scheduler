49861dd6b161d8d361b2db13193a135a1a9e0c03
Eric Neilsen <neilsen@fnal.gov>
2025-07-17 07:58:58 -0700

    stop-gap to support getting only sv visits

--- tests/utils/test_consdb.py
+++ tests/utils/test_consdb.py
@@ -13,6 +13,27 @@ from rubin_scheduler.utils.consdb import ConsDBVisits, load_consdb_visits
 class TestConsdb(unittest.TestCase):
 
     @unittest.skip("avoid requiring access to consdb for tests.")
+    def test_consdb_read_visits_lsstcam(self):
+        day_obs: str = "2025-07-01"
+        consdb_visits: ConsDBVisits = load_consdb_visits("lsstcam", day_obs)
+        schema_converter: SchemaConverter = SchemaConverter()
+
+        opsim: pd.DataFrame = consdb_visits.opsim
+        schema_converter.opsimdf2obs(opsim)
+
+        obs: np.recarray = consdb_visits.obs
+        schema_converter.obs2opsim(obs)
+
+    @unittest.skip("avoid requiring access to consdb for tests.")
+    def test_consdb_read_visits_with_constraints(self):
+        day_obs: str = "2025-07-01"
+        constraints = "band = 'z'"
+        consdb_visits: ConsDBVisits = load_consdb_visits("lsstcam", day_obs, constraints=constraints)
+        opsim: pd.DataFrame = consdb_visits.opsim
+        assert len(opsim) > 0
+        assert np.all(opsim.band == "z")
+
+    @unittest.skip("avoid requiring access to consdb for tests.")
     def test_consdb_read_visits_comcamsim(self):
         day_obs: str = "2024-06-26"
         consdb_visits: ConsDBVisits = load_consdb_visits("lsstcomcamsim", day_obs)
