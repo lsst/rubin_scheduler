fix bug in night computation

rubin_scheduler/utils/consdb.py
@@ -781,7 +781,11 @@ class ConsDBVisits(ABC):
         night : `int`
             The night of the survey.
         """
-        night: int = 1 + self.day_obs_mjd - int(np.floor(SURVEY_START_MJD))
+        night0_mjd: int = int(np.floor(SURVEY_START_MJD))
+        if night0_mjd > np.min(self.obs_start_mjd):
+            night0_mjd = np.min(self.obs_start_mjd)
+
+        night: int = 1 + (np.round(self.obs_start_mjd) - 0.5).astype(int) - int(np.floor(SURVEY_START_MJD))
         return night
 
     @cached_property