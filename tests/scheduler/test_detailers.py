f33ada94d6d3904b86a54f9e8252a67d751e6d23
Peter Yoachim <yoachim@uw.edu>
2025-11-20 17:23:09 -0800

    grab setting objects detailer

--- tests/scheduler/test_detailers.py
+++ tests/scheduler/test_detailers.py
@@ -57,6 +57,7 @@ class TestDetailers(unittest.TestCase):
             detailers.LabelDDFDetailer,
             detailers.TruncatePreTwiDetailer,
             detailers.IndexNoteDetailer,
+            detailers.GrabSettingDetailer,
         ]
 
         for det in det_list:
