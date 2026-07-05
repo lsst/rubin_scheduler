2501c91f71f57301da1c3682312362bb0b4dbf23
Lynne Jones <ljones.uw@gmail.com>
2026-07-05 20:14:58 +0200

    Move TargetofO and SimTargetooServer to their own files, with 'duration' default

--- rubin_scheduler/scheduler/utils/__init__.py
+++ rubin_scheduler/scheduler/utils/__init__.py
@@ -2,5 +2,6 @@ from .comcam_tessellate import *
 from .footprints import *
 from .observation_array import *
 from .sky_area import *
+from .too_objects import *
 from .tsp import *
 from .utils import *
